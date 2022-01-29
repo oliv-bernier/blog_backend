48c81da935fc422ad9319b2fcce4dface0df9f33
oliv-bernier <olivbernier@gmail.com>
2022-01-29 18:52:15 +0100

    update dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,7 @@ exports.signup = (req, res, next) => {
 					password: hash,
 					name: req.body.name,
 				});
+
 				user.save()
 					.then(() => res.status(201).json({ message: 'Utilisateur créé' }))
 					.catch(err => res.status(400).json({ error }));
@@ -23,11 +24,13 @@ exports.login = (req, res, next) => {
 			if (!user) {
 				return res.status(401).json({ error: 'Utilisateur non trouvé' });
 			}
+
 			bcrypt.compare(req.body.password, user.password)
 				.then(valid => {
 					if (!valid) {
 						return res.status(401).json({ error: 'Mot de passe incorrect' });
 					}
+					
 					res.status(200).json({
 						userId: user._id,
 						name: user.name,
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
+    
     if (req.body.userId && req.body.userId !== userId) {
       throw 'User Id incorrect';
     } else {
